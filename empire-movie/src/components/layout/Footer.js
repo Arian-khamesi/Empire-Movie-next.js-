6ce7186246595f069abf6642a672fc8a6c8a020e
Aryaun <ariankhamesi@gmail.com>
2024-04-28 22:37:30 +0330

    fixed footer

--- empire-movie/src/components/layout/Footer.js
+++ empire-movie/src/components/layout/Footer.js
@@ -1,9 +1,9 @@
-
+import styles from "./Footer.module.css"
 function Footer() {
   return (
-    <div className="footer-container">
-    <div className="footer container">
-    <h2 className='footer-logo'>EMPIRE MOVIE</h2>
+    <div className={styles.footer_container}>
+    <div className={`${styles.footer} container`}>
+    <h2 className={styles.footer_logo}>EMPIRE MOVIE</h2>
     <span>designed by Aryaun</span>
     </div>
   </div>
