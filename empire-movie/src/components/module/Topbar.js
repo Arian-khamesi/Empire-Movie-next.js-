733342401604c61a379f0c136020dff9f566d6bc
Aryaun <ariankhamesi@gmail.com>
2024-04-28 20:59:31 +0330

    fixed bugs of header style

--- empire-movie/src/components/module/Topbar.js
+++ empire-movie/src/components/module/Topbar.js
@@ -1,13 +1,14 @@
 import styles from "./Topbar.module.css"
 import Link from "next/link"
 
+
 function Topbar({ gradiant }) {
   return (
     <div className={gradiant ? `${styles.topbar_container} col-12` : `${styles.topbar_container_normal} col-12`}>
       <div className={styles.topbar}>
         <div className={styles.topbar_left}>
           <div className={styles.logo}>
-            <Link href={`/`}><img src="/images/empire.png" alt="logo" className={styles.logo_img} /></Link>
+            <Link href={`/`}><img src="./image/empire.png" alt="logo" className={styles.logo_img} /></Link>
           </div>
           <div className={styles.topbar_menu}>
             <Link href={"/show-all/movies/1"} className={styles.topbar_links}>Movies</Link>
