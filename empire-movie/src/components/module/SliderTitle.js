14836de53e86f23dc290f005537a3bfa748deb0a
Aryaun <ariankhamesi@gmail.com>
2024-04-30 17:02:40 +0330

    create component for titles

---
+++ empire-movie/src/components/module/SliderTitle.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import styles from "./SliderTitle.module.css"
+
+function SliderTitle({ title, btn, margin, href }) {
+    return (
+      <div className={margin ? `${styles.slider_title_container} ${styles.addMargin}` : styles.slider_title_container}>
+        <h2 className={styles.slider_title}>{title}</h2>
+        {btn ? <Link to={`/show-all/${href}/1`}><button className={styles.slider_all}>See All</button></Link> : null}
+      </div>
+    )
+  }
+  
+
+export default SliderTitle
