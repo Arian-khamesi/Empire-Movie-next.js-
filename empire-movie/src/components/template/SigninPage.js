a52bdf29b6659e23285a8c476306ae49218efa4a
Aryaun <ariankhamesi@gmail.com>
2024-05-02 20:44:30 +0330

    fixed log in page

--- empire-movie/src/components/template/SigninPage.js
+++ empire-movie/src/components/template/SigninPage.js
@@ -6,11 +6,14 @@ import { signIn } from "next-auth/react";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
 import Toast from "../module/Toast";
+import Loader from "../module/Loader";
+import 'react-toastify/dist/ReactToastify.css';
 
 function SigninPage() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loadeing, setLoading] = useState(false)
 
     const router = useRouter();
 
@@ -18,16 +21,38 @@ function SigninPage() {
     const signinHandler = async (e) => {
         e.preventDefault();
         console.log(email, password);
-
+        setLoading(true);
         const res = await signIn("credentials",
             {
                 email,
                 password,
                 redirect: false
             })
+        setLoading(false);
         if (res.error) {
-            console.log(res.error);
+            toast.error(res.error, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                // transition: Bounce,
+            });
         } else {
+            toast.success("You have successfully logged in", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                // transition: Bounce,
+            });
             router.push("/");
         }
     }
@@ -43,7 +68,10 @@ function SigninPage() {
                 <form action="signIn" className={styles.signin_form}>
                     <input className={styles.login_input} type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input className={styles.login_input} type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <button className={styles.login_user} onClick={signinHandler}>Log In</button>
+
+                    <div className={styles.loader}>
+                        {loadeing ? <Loader /> : <button className={styles.login_user} onClick={signinHandler}>Log In</button>}
+                    </div>
                 </form>
             </div>
             <div className={styles.top_footer}></div>
