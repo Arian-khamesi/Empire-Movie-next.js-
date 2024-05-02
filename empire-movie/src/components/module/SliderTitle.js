11e361208be81f1d4f14412081b56740b300710e
Aryaun <ariankhamesi@gmail.com>
2024-05-02 19:44:58 +0330

    create reusable component for sliders title

--- empire-movie/src/components/module/SliderTitle.js
+++ empire-movie/src/components/module/SliderTitle.js
@@ -5,7 +5,7 @@ function SliderTitle({ title, btn, margin, href }) {
     return (
       <div className={margin ? `${styles.slider_title_container} ${styles.addMargin}` : styles.slider_title_container}>
         <h2 className={styles.slider_title}>{title}</h2>
-        {btn ? <Link to={`/show-all/${href}/1`}><button className={styles.slider_all}>See All</button></Link> : null}
+        {btn ? <Link href={`/${href}`}><button className={styles.slider_all}>See All</button></Link> : null}
       </div>
     )
   }
