a11023a4ec972f2404483115b33e1d9190f80615
Aryaun <ariankhamesi@gmail.com>
2024-05-02 19:41:21 +0330

    add model for users

--- empire-movie/src/models/Users.js
+++ empire-movie/src/models/Users.js
@@ -1,4 +1,4 @@
-import { Schema,model,models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 
 const userSchema= new Schema ({
@@ -28,6 +28,6 @@ const userSchema= new Schema ({
     },
 })
 
-const User = models.User || model("User",userSchema)
+const User = models.User || model("User", userSchema)
 
 export default User
